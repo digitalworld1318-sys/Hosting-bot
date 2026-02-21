aa6d6be4551dd27caf7de93bbf7aa89ce98bea4b
digitalworld1318-sys <digitalworld1318@gmail.com>
2026-02-21 19:13:03 +0530

    Update Host.py

--- Host.py
+++ Host.py
@@ -45,7 +45,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration FROM .env FILE ---
-TOKEN = os.getenv('TOKEN', '')
+TOKEN = os.getenv('TOKEN', '8487143796:AAF0tiwQqFqnetxB2pngFo-1caBP-L7m18U')
 OWNER_ID = int(os.getenv('OWNER_ID', 7278872449 ))
 ADMIN_ID = int(os.getenv('ADMIN_ID', 7278872449))
 YOUR_USERNAME = os.getenv('YOUR_USERNAME', '@Z4X_Silent_Boy1')
