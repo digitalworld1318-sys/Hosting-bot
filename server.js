574bb738771357e5bad53e1640ba6c47b1b93779
digitalworld1318-sys <digitalworld1318@gmail.com>
2026-04-05 07:40:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,9 @@ const path = require('path');
 const fs = require('fs').promises;
 
 // Routes
-const tgRoute = require('./routes/tg');
 const keygenRoute = require('./routes/keygen');
 const deletekeyRoute = require('./routes/deletekey');
 const keysRoute = require('./routes/keys');
-const numRoute = require('./routes/num');
-const pakRoute = require('./routes/pak');
-const cnicRoute = require('./routes/cnic');
-const idRoute = require('./routes/id');
-const ifscRoute = require('./routes/ifsc');
-const upiRoute = require('./routes/upi');
-const upibombRoute = require('./routes/upibomb');
-const bombRoute = require('./routes/bomb');
-const familyRoute = require('./routes/family');
-const pincodeRoute = require('./routes/pincode');
-const ipinfoRoute = require('./routes/ipinfo');
-const vehicleRoute = require('./routes/vehicle');
-const challanRoute = require('./routes/challan');
 const gstRoute = require('./routes/gst');
 
 const { initializeKeyStore } = require('./utils/keyManager');
@@ -67,104 +53,19 @@ app.get('/', (req, res) => {
 
   "𖣘 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 𝗘𝗡𝗗𝗣𝗢𝗜𝗡𝗧𝗦 𖣘": {
 
-    "𖣘 𝗧𝗚 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/tg?key={Apka api key}&userid={user ID}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/tg?key=Z4X-ZH3XWZDZ-Silent&userid=7278872449"
-    },
-
-    "𖣘 𝗡𝗨𝗠 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/num?key={Apka api key}&number={mobile number}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/num?key=Z4X-ZH3XWZDZ-Silent&number=6202057758"
-    },
-
-    "𖣘 𝗣𝗔𝗞 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/pak?key={Apka api key}&number={pak number}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/pak?key=Z4X-ZH3XWZDZ-Silent&number=3122212427"
-    },
-
-    "𖣘 𝗖𝗡𝗜𝗖 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/cnic?key={Apka api key}&cnic={CNIC}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/cnic?key=Z4X-ZH3XWZDZ-Silent&cnic=4250182486429"
-    },
-
-    "𖣘 𝗜𝗗 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/id?key={Apka api key}&adhar={Aadhaar}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/id?key=Z4X-ZH3XWZDZ-Silent&adhar=594461916730"
-    },
-
-    "𖣘 𝗜𝗙𝗦𝗖 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/ifsc?key={Apka api key}&ifsc={IFSC}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/ifsc?key=Z4X-ZH3XWZDZ-Silent&ifsc=CBIN0280313"
-    },
-
-    "𖣘 𝗨𝗣𝗜 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/upi?key={Apka api key}&upi={UPI ID}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/upi?key=Z4X-ZH3XWZDZ-Silent&upi=paytmqrojt0q820zi@paytm"
-    },
-
-    "𖣘 𝗨𝗣𝗜𝗕𝗢𝗠𝗕 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/upibomb?key={Apka api key}&upi={UPI ID}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/upibomb?key=Z4X-ZH3XWZDZ-Silent&upi=paytmqrojt0q820zi@paytm"
-    },
-
-    "𖣘 𝗕𝗢𝗠𝗕 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/bomb?key={Apka api key}&number={mobile}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/bomb?key=Z4X-ZH3XWZDZ-Silent&number=9876543210"
-    },
-
-    "𖣘 𝗙𝗔𝗠𝗜𝗟𝗬 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/family?key={Apka api key}&adhar={Aadhaar}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/family?key=Z4X-ZH3XWZDZ-Silent&adhar=315736096255"
-    },
-
-    "𖣘 𝗣𝗜𝗡𝗖𝗢𝗗𝗘 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/pincode?key={Apka api key}&pincode={6 digit}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/pincode?key=Z4X-ZH3XWZDZ-Silent&pincode=110001"
-    },
-
-    "𖣘 𝗜𝗣𝗜𝗡𝗙𝗢 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/ipinfo?key={Apka api key}&ip={IP address}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/ipinfo?key=Z4X-ZH3XWZDZ-Silent&ip=8.8.8.8"
-    },
-
-    "𖣘 𝗩𝗘𝗛𝗜𝗖𝗟𝗘 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/vehicle?key={Apka api key}&reg={registration number}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/vehicle?key=Z4X-ZH3XWZDZ-Silent&reg=MH14KK9159"
-    },
-
-    "𖣘 𝗖𝗛𝗔𝗟𝗟𝗔𝗡 𖣘": {
-      "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/challan?key={Apka api key}&vehicle={vehicle number}",
-      "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/challan?key=Z4X-ZH3XWZDZ-Silent&vehicle=UP70AJ2399"
-    },
-
     "𖣘 𝗚𝗦𝗧 𖣘": {
       "❖ 𝗔𝗣𝗜 𝗘𝗡𝗗 𝗣𝗢𝗜𝗡𝗧 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 ❖": "/gst?key={Apka api key}&gst={GST number}",
       "𖣘 𝗘𝗫𝗔𝗠𝗣𝗟𝗘 𖣘": "/gst?key=Z4X-ZH3XWZDZ-Silent&gst=27AAPFU0939F1ZV"
     }
-  }
 }`;
   res.setHeader('Content-Type', 'application/json');
   res.send(jsonString);
 });
 
 // Register all API routes (including hidden admin ones)
-app.use('/tg', tgRoute);
 app.use('/keygen', keygenRoute);
 app.use('/deletekey', deletekeyRoute);
 app.use('/keys', keysRoute);
-app.use('/num', numRoute);
-app.use('/pak', pakRoute);
-app.use('/cnic', cnicRoute);
-app.use('/id', idRoute);
-app.use('/ifsc', ifscRoute);
-app.use('/upi', upiRoute);
-app.use('/upibomb', upibombRoute);
-app.use('/bomb', bombRoute);
-app.use('/family', familyRoute);
-app.use('/pincode', pincodeRoute);
-app.use('/ipinfo', ipinfoRoute);
-app.use('/vehicle', vehicleRoute);
-app.use('/challan', challanRoute);
 app.use('/gst', gstRoute);
 
 // 404 handler
